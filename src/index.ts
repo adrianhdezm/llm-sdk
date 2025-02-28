c536068f271971e73ebe8b5bd8a46a8be1aa8838
Adrian Hernandez-Mendez <aahdezm@gmail.com>
2025-02-28 23:09:18 +0100

    Re-export validation helpers and data models in index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ export * from './generation/generate-text';
 
 // Re-export helpers
 export * from './helpers/function-tool.helpers';
+export * from './helpers/validation.helpers';
 
 // Re-export models
 export * from './models/llm-message.models';
 export * from './models/llm-tool.models';
+export * from './models/data.models';
 export * from './models/llm.models';
 
 // Re-export services
